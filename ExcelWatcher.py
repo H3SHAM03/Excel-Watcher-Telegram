b8f90f64a8f0d444d980a353a60d64012ea31b21
AbdelRahman Hesham <dollarouty@hotmail.com>
2025-01-29 15:26:30 +0200

    fixes

--- ExcelWatcher.py
+++ ExcelWatcher.py
@@ -16,10 +16,11 @@ def get_args(message):
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
-    user_data[f'{message.chat.id}'] = []
+    user_data[f'{message.from_user.id}'] = []
     bot.reply_to(message, f"Hi {message.from_user.first_name}, how are you doing?")
     if message.from_user.username not in users:
         users.append(message.from_user.username)
+        save_data()
 
 @bot.message_handler(commands=['add'])
 def add_excel_link(message):
@@ -39,6 +40,7 @@ def add_excel_link(message):
             row_count = len(df.axes[0])
             user_data[f'{message.from_user.id}'].append({'name': name, 'link': link, 'row_count': row_count, 'username': message.from_user.username})
             bot.reply_to(message, f"File {name} added successfully")
+            save_data()
     except Exception as e:
         print(e)
         bot.reply_to(message,"Usage: /add <link> <name>")
@@ -47,13 +49,16 @@ def check_list():
     while True:
         for key in list(user_data.keys()):
             for i in user_data[key]:
-                response = requests.get(i['link'])
-                df = pd.read_excel(BytesIO(response.content))
-                if len(df.axes[0]) > i['row_count']:
-                    bot.send_message(key,f"Bad News!\nFile {i['name']} has been updated from {i['row_count']} to {len(df.axes[0])}")
-                elif len(df.axes[0]) < i['row_count']:
-                    bot.send_message(key,f"Good News!\nFile {i['name']} has been updated from {i['row_count']} to {len(df.axes[0])}")
-                i['row_count'] = len(df.axes[0])
+                try:
+                    response = requests.get(i['link'])
+                    df = pd.read_excel(BytesIO(response.content))
+                    if len(df.axes[0]) > i['row_count']:
+                        bot.send_message(key,f"Bad News!\nFile {i['name']} has been updated from {i['row_count']} to {len(df.axes[0])}")
+                    elif len(df.axes[0]) < i['row_count']:
+                        bot.send_message(key,f"Good News!\nFile {i['name']} has been updated from {i['row_count']} to {len(df.axes[0])}")
+                    i['row_count'] = len(df.axes[0])
+                except Exception as e:
+                    print(e)
                 
 
 @bot.message_handler(commands=['list'])
@@ -78,15 +83,39 @@ def remove_excel_link(message):
             if i != get_args(message.text)[-1]:
                 name += ' '
         for i in user_data[f'{message.from_user.id}']:
+            if i['name'] == name:
                 user_data[f'{message.from_user.id}'].remove(i)
                 bot.reply_to(message,f"File {name} removed successfully")
+                save_data()
                 return
         bot.reply_to(message,f"File {name} not found in list")
     except Exception as e:
         print(e)
         bot.reply_to(message,"Usage: /remove <name>")
-            
 
+def save_data():
+    try:
+        with open('user data.txt','w') as f:
+            f.write(str(user_data))
+    except Exception as e:
+        print(e)
+
+def read_data():
+    try:
+        f = open('user data.txt','r')
+    except:
+        open('user data.txt','x')
+        f = open('user data.txt','r')
+    data = f.read()
+    global user_data
+    if data == '':
+        user_data = {}
+        f.close()
+    else:
+        user_data = eval(data)
+        f.close()        
+
+read_data()
 t = threading.Thread(target=check_list)
 t.start()
 
